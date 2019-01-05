c4475b759450c248d9bf0c454baf73be90837537
Rusty Bower <rusty@rustybower.com>
2019-01-05 04:21:14 -0600

    Fixing coveralls

---
+++ tests/test_weather.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+import unittest
+
+from sopel_modules.weather import weather
+
+
+class TestWeather(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testSomething(self):
+        pass
